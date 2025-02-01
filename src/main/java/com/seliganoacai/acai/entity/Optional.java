10e1dfc1ce080b1535e74ec340f894335e8b6140
briciosvieira <fabricio_9327@hotmail.com>
2025-02-01 14:49:52 -0300

    CREATE class optional and Product

---
+++ src/main/java/com/seliganoacai/acai/entity/Optional.java
@@ -0,0 +1,42 @@
+package com.seliganoacai.acai.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Optional implements Serializable {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String optional;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Optional optional = (Optional) o;
+        return Objects.equals(id, optional.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/seliganoacai/acai/entity/Product.java
@@ -0,0 +1,90 @@
+package com.seliganoacai.acai.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(EntityListeners.class)
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String ml;
+    @Column
+    private int quantity;
+    @Column
+    private double value;
+    @Column
+    @ManyToMany
+    @JoinTable(
+            name = "product_options",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "option_id")
+    )
+    private List<Optional> optional;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMl() {
+        return ml;
+    }
+
+    public void setMl(String ml) {
+        this.ml = ml;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public List<Optional> getOptional() {
+        return optional;
+    }
+
+    public void setOptional(List<Optional> optional) {
+        this.optional = optional;
+    }
+
+}
